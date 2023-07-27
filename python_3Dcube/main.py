69b296b732fc2c7938ba775188614ef0f7f62ab5
togg12 <140746447+togg12@users.noreply.github.com>
2023-07-27 19:59:35 +0200

    Add files via upload

---
+++ python_3Dcube/main.py
@@ -0,0 +1,40 @@
+import turtle as t
+
+def cube():
+    t.color("blue")
+    t.left(270)
+    t.forward(100) # 1
+    t.color("red")
+    t.left(90)
+    t.forward(100) # 2
+    t.color("yellow")
+    t.left(90)
+    t.forward(100) # 3
+    t.color("green")
+    t.left(90)
+    t.forward(100) # 4
+    t.color("purple")
+    t.right(135)
+    t.forward(45) # 5
+    t.color("orange")
+    t.right(45)
+    t.forward(100) # 6
+    t.color("pink")
+    t.right(135) 
+    t.forward(45) # 7
+    t.color("yellow")
+    t.left(45)
+    t.forward(100) # 8
+    t.color("peru")
+    t.left(135)
+    t.forward(50) # 9
+    t.color("medium purple")
+    t.left(45)
+    t.forward(100) # 10
+    
+
+
+
+cube()
+
+input("Input... ")
